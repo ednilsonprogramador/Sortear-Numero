01956240be841b5cd14554f91b867f35319722b0
Ednilson Santos <n1njabpt@gmail.com>
2026-04-21 09:43:37 -0300

    adição alert

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function sortear() {
     const min = Math.ceil(document.getElementById("Min").value);
     const max = Math.floor(document.getElementById("Max").value);
-    
-    const resultado = Math.floor(Math.random() * (max - min + 1)) + parseInt(min);
-    alert("O número sorteado é: " + resultado);
+
+
+    if (min >= max) {
+
+        alert("O número máximo deve ser maior que o mínimo.");
+    }
+    else {
+        const resultado = Math.floor(Math.random() * (max - min + 1)) + parseInt(min);
+        alert("O número sorteado é: " + resultado);
+    }
 }
